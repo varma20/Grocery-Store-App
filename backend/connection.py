77b3652638d1fe212652e1806d1bd5800a3378d0
gunasekhar20 <gunasekhar.sangaraju@gmail.com>
2024-02-23 19:21:50 +0530

    modified

--- backend/connection.py
+++ backend/connection.py
@@ -5,7 +5,7 @@ __cnx = None
 def sql_connection():
     global __cnx
     if __cnx is None:
-         cnx = mysql.connector.connect(user='root', password='Varma',
+        __cnx = mysql.connector.connect(user='root', password='Varma',
                                 host='127.0.0.1',
                                 database='gs')
     return __cnx
--- backend/products.py
+++ backend/products.py
@@ -20,6 +20,25 @@ def get_all_products(connection):
 
     connection.close()
     return response
+
+def insert_a_product(connection,product):
+    cursor = connection.cursor()
+
+    query = ("INSERT INTO products""(name, units_of_measure_id, price_per_unit)""VALUES (%s,%s,%s)")
+    
+    data = (product['product_name'],product['units_of_measure_id'],product['price_per_unit'])
+    cursor.execute(query,data)
+    connection.commit()
+
+    return cursor.lastrowid
+
+def delete_a_row(connection,product_id):
+    cursor = connection.cursor()
+    query = ("DELETE FROM products where product_id =" + str(product_id))
+    cursor.execute(query)
+    connection.commit()
+
+
 if __name__ == "__main__":
     connection = sql_connection()
-    print(get_all_products(connection))
+    print(delete_a_row(connection,5))
